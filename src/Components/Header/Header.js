83154b361e5abdd7bf7cb09013dae7029794b9eb
sergiopesoa <107009985+sergiopesoa@users.noreply.github.com>
2022-09-23 19:14:00 -0300

    primeros cambios

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react"
+import logo from "../../assets/logo.jpg"
+const Header = () => {
+    return(
+        <header>
+            <img src={logo} alt="tienda online"/>
+            <h1>titulo</h1>
+            <nav>
+                <a href="">categoria 1</a>
+                <a href="">categoria 2</a>
+                <a href="">categoria 3</a>
+                <a href="">categoria 4</a>
+            </nav>
+            <img src="" alt=""/>
+
+
+
+        </header>
+    )
+}
+export default Header
---
+++ src/info.txx
@@ -0,0 +1,6 @@
+Componentes funcionales
+-importar a react(opcional)
+-debe tener una funcion
+-el nombre de la funcion debe empezar con mayusculas SIEMPRE
+-la funcion debe retornar algo(siempre debe ser un solo elemento padre)
+-se debe exportar la funcion
