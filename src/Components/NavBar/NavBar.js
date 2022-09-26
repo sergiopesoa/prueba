ee49f5f266311bf67dd097bf2bbaf1ad24af7ab1
sergiopesoa <107009985+sergiopesoa@users.noreply.github.com>
2022-09-26 18:13:26 -0300

    modificacion NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,55 +1,40 @@
 import React from "react"
-import Navbar from "react-bootstrap/Navbar";
+import Navbar from "bootstrap"
 import './NavBar.css'
 import logo from "../../assets/logo.jpg"
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-const Header = () => {
+const NavBar = () => {
     return (
         <header>
             <img src={logo} alt="tienda online"/>
             <h1> Tienda Online</h1>
-            
-                <nav class="navbar navbar-expand-lg bg-light">
+            <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Como comprar</a>
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Vajilla</a>
+          <a class="nav-link" href="#">Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Cocina</a>
+          <a class="nav-link" href="#">Pricing</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Café y Te
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Tazas</a></li>
-            <li><a class="dropdown-item" href="#">Cafeteras y Teteras</a></li>
-            <li><a class="dropdown-item" href="#">juegos completos</a></li>
-          </ul>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
         </li>
       </ul>
     </div>
   </div>
-
- 
 </nav>
-                
-                
-                
-            
-        
 
-
-
+   
+    
 
             
             <ShoppingCartIcon color="primary" fontSize="large"/>
@@ -57,6 +42,7 @@ const Header = () => {
 
 
         </header>
-    )
+    );
 }
-export default Header
+
+export default NavBar
